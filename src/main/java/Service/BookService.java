2e5479e64f675f81be25389c29140dcd28b08e19
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-02 00:26:29 +0500

    feat: fetch books from Google Books API

--- src/main/java/Service/BookService.java
+++ src/main/java/Service/BookService.java
@@ -2,17 +2,13 @@ package Service;
 
 import Model.Book;
 import lombok.SneakyThrows;
-import netscape.javascript.JSObject;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
