d324e25062713d30f027ecab5c9c775e0aa6592e
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-01 23:37:46 +0500

    feat(Back-end): url connection

--- src/main/java/Service/BookService.java
+++ src/main/java/Service/BookService.java
@@ -1,9 +1,26 @@
 package Service;
 
 import Model.Book;
+import lombok.SneakyThrows;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class BookService {
+    @SneakyThrows
     public Book searchBook(String query) {
-        String url = 
+        String apiUrl = "https://www.googleapis.com/books/v1/volumes?q=" + query;
+
+        URL url = new URL(apiUrl);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        urlConnection.connect();
     }
 }
+
+
+
+
+
