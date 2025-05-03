43cd321b946a0b74092cdc5a1bbbcb221cf64e83
bekhruzdev01 <bhamdamov3121@gmail.com>
2025-05-03 22:35:24 +0500

    feat: fetch books from Google Books API

--- src/main/java/Service/DbService.java
+++ src/main/java/Service/DbService.java
@@ -1,4 +1,20 @@
 package Service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DbService {
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/first-jsp";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "root123";
+
+    public Connection getConnection() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
